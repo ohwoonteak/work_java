ec688389b18e924485d3f8a2f631444d9eece13d
KOITT <KOITT@KOITT-PC02>
2018-01-04 13:35:11 +0900

    5장 연습문제 9번10번

--- ch05/src/practice/no10.java
+++ ch05/src/practice/no10.java
@@ -2,5 +2,28 @@ package practice;
 
 public class no10 {
 
+	public final String[] osType = {"윈도7","애플 OS X","안드로이드"};
+	
+	private int indexOfOs;		//osType의 인덱스 
+	private int memory;			//메모리 크기
+		//생성자
+	public no10(int indexOfOs, int memorry) {
+		this.indexOfOs = indexOfOs;
+		this.memory = memorry;
+	}
+	public void print() {
+		System.out.println("운영체제: " + osType[this.indexOfOs] + ", 메인메모리: " + this.memory);
+	}
+
+	public static void main(String[] args) {
+		no10 pc = new no10(0,16);
+		no10 apple = new no10(1,32);
+		no10 galaxy = new no10(2,16);
+		pc.print();
+		apple.print();
+		galaxy.print();
+		
+	}
+	
 
 }
--- ch05/src/practice/no9.java
+++ ch05/src/practice/no9.java
@@ -2,4 +2,21 @@ package practice;
 
 public class no9 {
 
+		private double width;
+		private double height;
+		
+		public no9(double width,double height) {
+			this.width = width;
+			this.height = height;
+			
+		}
+		
+		public double getArea() {
+			return (this.width *this.height)*2;
+		}
+		public static void main(String[] args) {
+			no9 rc = new no9(3.82,8.65);
+			System.out.println("면적: " + rc.getArea());
+			
+		}
 }
